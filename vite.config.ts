59d5b16a1f8e2c0fe9cc43fcf0f3a3d2cae99172
alex-go-nguyen <alex.nguyen.goldenowl@gmail.com>
2023-08-29 16:43:59 +0700

    Add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'golden-owl-solutions-d84caee41',
+      project: 'booking-field-fe',
+    }),
+  ],
   server: {
     port: 3001,
   },
