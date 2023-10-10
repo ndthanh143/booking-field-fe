820bdc2012f103125f80d40d82509b172fb227cc
alex-go-nguyen <alex.nguyen.goldenowl@gmail.com>
2023-10-10 17:20:16 +0700

    fix noti

--- src/services/notification/notification.query.ts
+++ src/services/notification/notification.query.ts
@@ -7,6 +7,9 @@ export const notificationKeys = {
   lists: () => [...notificationKeys.all, 'list'] as const,
   list: (query: getNotificationsQuery) =>
     defineQuery([...notificationKeys.lists()], () => notificationService.getAll(query)),
+  currentUsers: () => [...notificationKeys.all, 'list'] as const,
+  currentUser: (query: getNotificationsQuery) =>
+    defineQuery([...notificationKeys.currentUsers()], () => notificationService.getByCurrentUser(query)),
   details: () => [...notificationKeys.all, 'detail'] as const,
   detail: (id: number) => defineQuery([...notificationKeys.details(), id], () => notificationService.getOne(id)),
   countNotSeens: () => [...notificationKeys.all, 'count-not-seen'] as const,
